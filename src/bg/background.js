f8ea3fe329f52cc2dc8f98ff4a7804c17eb81c30
ThePaavero <pekka@astudios.org>
2016-11-26 20:10:20 +0200

    IC

---
+++ src/bg/background.js
@@ -0,0 +1,9 @@
+chrome.contextMenus.create({
+  'title': 'Search in Google Maps',
+  'contexts': ['selection'],
+  'onclick': (e) => {
+    const selectedText = e.selectionText
+    const url = 'https://www.google.fi/maps?q=' + encodeURIComponent(selectedText) + '&um=1&ie=UTF-8&sa=X'
+    window.open(url)
+  }
+});
