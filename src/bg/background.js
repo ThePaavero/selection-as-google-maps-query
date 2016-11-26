14fb4d69f66adb3e8c5798cbafb3194f5fd195e9
ThePaavero <pekka@astudios.org>
2016-11-26 20:22:19 +0200

    No quotes on object keys

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,7 +1,7 @@
 chrome.contextMenus.create({
-  'title': 'Search in Google Maps',
-  'contexts': ['selection'],
-  'onclick': (e) => {
+  title: 'Search in Google Maps',
+  contexts: ['selection'],
+  onclick: (e) => {
     const selectedText = e.selectionText
     const url = 'https://www.google.com/maps?q=' + encodeURIComponent(selectedText) + '&um=1&ie=UTF-8&sa=X'
     window.open(url)
